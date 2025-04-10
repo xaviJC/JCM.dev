ac7ffed2283781566d161d33a88f365a9bbdf6c2
xaviJC <javierhyws@gmail.com>
2025-04-10 14:43:26 +0200

    Configure GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://xavijc.github.io',
+  base: '/JCM.dev',
   integrations: [tailwind()],
 }); 
